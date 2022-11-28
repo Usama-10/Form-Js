bb44c676733893c5d2e16648389b96b499e23888
Usama-10 <usamaisthiaq768@gmail.com>
2022-11-28 08:04:38 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function returnInput() {
+    var a = document.getElementById("Fname").value
+    var b = document.getElementById("Lname").value
+    var c = document.getElementById("num").value
+    var d = document.getElementById("num_2").value
+    var e = document.getElementById("mail").value
+    var f = document.getElementById("pass").value
+    
+    
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
+
+}
+
